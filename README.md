c14f1d71a94fc1ee8c3206487ac429dc02d1e9a1
kipper0508 <you@example.com>
2020-09-26 19:09:37 +0800

    first version for new website

---
+++ README.md
@@ -0,0 +1,75 @@
+# Auto_NCU-CSIE_Classroom_Reservation
+
+### Dependency
+* Python3
+* requests
+* lxml
+
+### Usage
+```shell
+usage: python3 clock.py [-h] [--config CONFIG] [-c CLASSROOM]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --config CONFIG       default: config.json
+  -c CLASSROOM, --classroom CLASSROOM       classrooms are defined in config.json (default: default_room)
+``` 
+
+### How to Use
+* Make sure you have installed the dependencies.
+    ```shell
+    pip3 install requests lxml
+    ```
+* Create config.json
+    ```shell
+    cp config.json.example config.json
+    ```
+* Set these parameters in config.json
+    * portal account&password
+    * cellphone number
+    * teacher's name
+    * date
+        * only can reserve 5 days early
+        * "sign_online_day" : "using_classroom_day"
+    * cid table
+        * A203 : 6
+        * A204 : 7
+        * A205 : 8
+        * A206 : 9
+        * A207 : 11
+        * 208 : 12
+        * A209 : 13
+        * A210 : 14
+        * A211 : 15
+        * A301 : 17
+        * A302 : 18
+        * A303 : 25
+        * A306 : 26
+        * B217 : 27
+        * B223 : 23
+        * B226 : 21
+        * B323 : 24 
+        * B326 : 22
+    * start&end period table
+        * 0 : "08:00-08:50",
+        * 1 : "09:00-09:50",
+        * 2 : "10:00-10:50",
+        * 3 : "11:00-11:50",
+        * 4 : "12:00-12:50",
+        * 5 : "13:00-13:50",
+        * 6 : "14:00-14:50",
+        * 7 : "15:00-15:50",
+        * 8 : "16:00-16:50",
+        * 9 : "17:00-17:50",
+        * 10 : "18:00-18:50",
+        * 11 : "19:00-19:50",
+        * 12 : "20:00-20:50",
+        * 13 : "21:00-21:50"
+
+* Set the script executable.
+    ```shell
+    $ sudo chmod 777 Auto_clock/clock.py
+    ```
+
+
+
---
+++ clock.py
@@ -0,0 +1,104 @@
+import requests
+import argparse
+import json
+import os
+from lxml import html
+import datetime
+
+parser = argparse.ArgumentParser('python3 clock.py')
+parser.add_argument("--config", default="config.json", help="default: %(default)s")
+parser.add_argument("-c", "--classroom", default="default_room", help="classrooms are defined in config.json. default: %(default)s ")
+args = parser.parse_args()
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+def main():
+    with open(args.config) as config_file:
+        config = json.load(config_file)
+
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection': 'keep-alive',
+        'Host': 'portal.ncu.edu.tw',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+    }
+
+    session = requests.session()
+
+    login(session, headers, config['account'], config['password'])
+
+    sign(session, config["rooms"][args.classroom])
+    
+
+def login(session, headers, account, password):
+    payload = {
+        'language' : 'CHINESE',
+        'username' : account,
+        'password' : password
+    }
+
+    r = session.get('https://portal.ncu.edu.tw/login', headers=headers)
+
+    # Get CSRF token from portal login page
+    tree = html.fromstring(r.text)
+    payload['_csrf'] = tree.forms[0].fields['_csrf']
+
+    r = session.post('https://portal.ncu.edu.tw/login', data=payload)
+    
+    r = session.get('https://classroom.csie.ncu.edu.tw/reserve.html')    #This whill redirect to https://classroom.csie.ncu.edu.tw/index.html
+    # Get CSRF token from portal redirect form
+    tree = html.fromstring(r.text)
+    token = tree.forms[0].fields['_csrf']
+
+    r = session.post('https://portal.ncu.edu.tw/leaving', data={ "_csrf" : token })
+
+    payload2 = {
+        'approval'  : "true"
+    }
+
+    # Get CSRF token from portal redirect form
+    tree = html.fromstring(r.text)
+    payload2['_csrf'] = tree.forms[0].fields['_csrf']
+
+    r = session.post('https://api.cc.ncu.edu.tw/oauth/oauth/confirm', data=payload2)
+    #client_id=YzE2YzUxYjQtNDk2NS00NjRjLWJlNjktNzk1ZDkyMDVkZDhl ,client = what system you want to use
+
+    return
+
+def sign(session, classroom):
+    Sign = {
+        'cid': classroom["cid"],
+        'phone': classroom["phone"],
+         'teacher': classroom["teacher"],
+         'start_period': classroom["start_period"],
+         'end_period':  classroom["end_period"],
+         "note": classroom["note"]
+    }
+    today=datetime.date.today().strftime("%Y-%m-%d")
+    use_day=classroom["date"][today]+" 00:00:00"
+    Sign['date']=use_day
+
+    headers2 = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+        'Connection': 'keep-alive',
+        'Host': 'course.csie.ncu.edu.tw',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+    }
+
+    r = session.get('https://course.csie.ncu.edu.tw/reserve.html',headers=headers2)
+
+    # Sign
+    r = session.post('https://course.csie.ncu.edu.tw/reserve', data=Sign)
+    
+    return
+
+if __name__ == '__main__':
+    main()
